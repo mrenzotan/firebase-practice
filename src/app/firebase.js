35860c999b7cbcb788353c3d4096423335fc982a
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-10-19 17:10:30 +0800

    chore: setup firebase authentication, firestore, cloud storage

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6mlXQZWHd7TBYu5-kMdriZ6ssdweQQpU',
+  authDomain: 'friendlyeats-codelab-21861.firebaseapp.com',
+  projectId: 'friendlyeats-codelab-21861',
+  storageBucket: 'friendlyeats-codelab-21861.appspot.com',
+  messagingSenderId: '632615373124',
+  appId: '1:632615373124:web:66684b336598cf951d2f11',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
